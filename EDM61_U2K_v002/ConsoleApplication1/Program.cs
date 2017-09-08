7434fe7ee047df656df781fb5d4e0e3eb2f8ea31
Paul Chang <pchang@rs.com>
2017-09-08 10:25:16 -0600

    EDM 6.1.3 sample code

---
+++ EDM61_U2K_v002/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("start");
+                CustomerContext ctx = new CustomerContext();
+                var t = ctx.Database.SqlQuery<Customer>("SELECT CUSTID,FNAME,LNAME,FULLADDR FROM CUSTOMER");
+                foreach (Customer item in t)
+                {
+                    Console.WriteLine(item.CUSTID + "=>" + item.FNAME + "=>" + item.LNAME + "=>" + item.FULLADDR);
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.InnerException != null)
+                { Console.WriteLine(e.InnerException.Message); }
+                else
+                { Console.WriteLine(e.Message); }
+            }
+            finally
+            {
+                Console.WriteLine("Enter to exit:");
+                string line = Console.ReadLine();
+            }
+        }
+    }
+}
